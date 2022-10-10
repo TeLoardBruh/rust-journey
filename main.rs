e8f94583c2545ed06b6774af7ba70617d59329f7
TeLoardBruh <damnloardbruh@gmail.com>
2022-10-10 20:14:54 +0700

    feat: learning about string

--- main.rs
+++ main.rs
@@ -2,8 +2,16 @@ fn main() {
     let x = plus_one(5);
 
     println!("The value of x is: {x}");
+    test_str();
 }
 
 fn plus_one(x: i32) -> i32 {
     x + 1
+}
+
+fn test_str() {
+    let mut s = String::from("hello");
+    s.push_str(", world!");
+
+    println!("{}", s);
 }
